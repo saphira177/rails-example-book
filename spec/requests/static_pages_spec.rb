a9aa57d7687529ecf7c6efb32d564f0227758f4e
anhdungle1986 <anhdungle1986@gmail.com>
2013-01-17 01:17:06 +0700

    Finish Layout and Routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+	subject { page }
   describe "Home page" do
+  	before { visit root_path }
+    
+    it { should have_selector('title', text: "RailsExample | Home") }
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', text: "RailsExample | Home")
-    end
   end
 end
